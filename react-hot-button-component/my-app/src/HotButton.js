a1d8fb044d33ab6294fd44f41e37929876a854b0
Macy Hong <macyhong@gmail.com>
2023-04-16 09:32:51 +0000

    Removed commented out code

--- react-hot-button-component/my-app/src/HotButton.js
+++ react-hot-button-component/my-app/src/HotButton.js
@@ -2,26 +2,10 @@ import { useState } from 'react';
 
 export default function HotButton() {
   const [clicks, setClicks] = useState(0);
-  console.log('Current number of clicks: ', clicks)
 
   function handleClick(){
     setClicks(clicks + 1);
   }
-
-  // var buttonColor;
-  // if (clicks <= 3) {
-  //   buttonColor = 'dark-purple'
-  // } else if (clicks <= 6) {
-  //   buttonColor = 'light-purple'
-  // } else if (clicks <= 9) {
-  //   buttonColor = 'coral'
-  // } else if (clicks > 9 && clicks <= 12) {
-  //   buttonColor = 'orange'
-  // } else if (clicks > 12 && clicks <= 15) {
-  //   buttonColor = 'yellow'
-  // } else {
-  //   buttonColor = 'white'
-  // }
 
   const buttonColor = clicks <= 3 ? 'dark-purple'
   : clicks <= 6 ? 'light-purple'
